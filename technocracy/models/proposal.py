54c810206d703eb97c03987f41b526aef4aa9514
Rico Rodriguez <rico.l.rodriguez@gmail.com>
2016-06-26 21:24:58 -0700

    Fleshed out Citizen model, added pip-tools dependency management

--- technocracy/models/proposal.py
+++ technocracy/models/proposal.py
@@ -2,6 +2,13 @@ from ..webapp import database as db
 
 
 class Proposal(db.Model):
+    """
+    Proposals.
+
+    TODO(rico): Flesh out the proposal framework.
+    """
+
+    __tablename__ = 'proposals'
 
     id = db.Column(db.Integer, primary_key=True)
     # Additional fields
--- technocracy/webapp.py
+++ technocracy/webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy  # declarative_base
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy_jsonapi import FlaskJSONAPI
 
@@ -10,9 +10,6 @@ app = Flask(__name__)
 
 # 2) Set up the database. We can now import this anywhere.
 database = SQLAlchemy(app)
-
-# Declarative base needs to be hooked up to the correct engine...
-# OrmBase = declarative_base()
 
 
 def setup_app(config_name, app, database):
